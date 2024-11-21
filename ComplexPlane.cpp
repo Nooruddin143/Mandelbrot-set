11bebaf6f21a5e37d8455a01419a49d9a958805d
Nooruddin Mohammadi <nooruddinmohammadi63@gmail.com>
2024-11-21 13:47:01 -0800

    compiled successfully

--- ComplexPlane.cpp
+++ ComplexPlane.cpp
@@ -1,1 +1,34 @@
 #include "ComplexPlane.h"
+using namespace std;
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight)
+{
+	m_pixel_size = { pixelWidth, pixelHeight };
+}
+void ComplexPlane::draw(RenderTarget& target, RenderStates states) const
+{
+
+}
+void ComplexPlane::zoomIn()
+{
+
+}
+void ComplexPlane::zoomOut()
+{
+
+}
+void ComplexPlane::setCenter(Vector2i mousePixel)
+{
+
+}
+void ComplexPlane::setMouseLocation(Vector2i mousePixel)
+{
+
+}
+void ComplexPlane::loadText(Text& text)
+{
+
+}
+void ComplexPlane::updateRender()
+{
+
+}
