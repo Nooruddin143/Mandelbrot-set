80528e77659b47df438f8d0538292dedee1e8cf4
Nooruddin Mohammadi <nooruddinmohammadi63@gmail.com>
2024-11-24 22:47:28 -0800

    new comment

--- ComplexPlane.h
+++ ComplexPlane.h
@@ -7,8 +7,8 @@
 #include <sstream>
 using namespace sf;
 using namespace std;
-
-const unsigned int MAX_ITER = 64;
+//all libs
+const unsigned int MAX_ITER = 5000;
 const float BASE_WIDTH = 4.0;
 const float BASE_HEIGHT = 4.0;
 const float BASE_ZOOM = 0.5;
